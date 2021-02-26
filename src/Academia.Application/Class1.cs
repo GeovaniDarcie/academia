f2d1df29a685c6d48bf431f33a98b7dd01c4aa25
geovaniDarcie <geovani_darcie@hotmail.com>
2021-02-26 18:40:50 -0300

    Estrutura criada

---
+++ src/Academia.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Academia.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Academia.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Academia.Infra
+{
+    public class Class1
+    {
+    }
+}
