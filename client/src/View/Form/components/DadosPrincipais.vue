3d816dca97efa2b6e4485379fbbe445ac0c77547
geovaniDarcie <geovani_darcie@hotmail.com>
2021-03-24 11:56:35 -0300

    gitignore

--- client/src/View/Form/components/DadosPrincipais.vue
+++ client/src/View/Form/components/DadosPrincipais.vue
@@ -1,79 +1,104 @@
 <template>
   <div>
-    <b-form inline class="mt-3 ml-3">
+    <b-form class="inline">
       <b-form-group id="input-group-1" label="Nome: " label-for="input-1">
-         <b-form-input
+        <b-form-input
           id="input-1"
           v-model="nome"
-          class="mr-2 ml-2"
           placeholder="Digite seu nome"
         ></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-2" label="Sobrenome: " label-for="input-2">
-         <b-form-input
+        <b-form-input
           id="input-2"
           v-model="sobrenome"
           class="mr-2 ml-2"
           placeholder="Digite seu sobrenome"
         ></b-form-input>
       </b-form-group>
+    </b-form>
+    <b-form-group
+      id="input-group-3"
+      label="Data de Nascimento: "
+      label-for="input-3"
+    >
+      <b-form-input
+        id="input-3"
+        v-model="dataDeNascimento"
+        type="date"
+        class="mr-2 ml-2"
+        placeholder="Data de nascimento: "
+      ></b-form-input>
+    </b-form-group>
 
-      <b-form-group id="input-group-3" label="Data de Nascimento: " label-for="input-3">
-         <b-form-input
-          id="input-3"
-          v-model="dataDeNascimento"
-          class="mr-2 ml-2"
-          placeholder="Digite sua data de nascimento"
-        ></b-form-input>
-      </b-form-group>
-      
-      <b-form-group id="input-group-4" label="Sexo: " label-for="input-4">
-         <b-form-group
-          id="input-4"
-          v-model="sexo"
-          class="mr-2 ml-2"
-        ></b-form-group>
-      </b-form-group>
+    <b-form-group id="input-group-4" label="Sexo: " label-for="input-4">
+      <b-form-input
+        id="input-4"
+        v-model="sexo"
+        class="mr-2 ml-2"
+        placeholder="Sexo: "
+      ></b-form-input>
+    </b-form-group>
 
-      <b-form-group id="input-group-5" label="Cpf: " label-for="input-5">
-         <b-form-group
-          id="input-5"
-          v-model="cpf"
-          class="mr-2 ml-2"
-        ></b-form-group>
-      </b-form-group>
+    <b-form-group id="input-group-5" label="Cpf: " label-for="input-5">
+      <b-form-input
+        id="input-5"
+        v-model="cpf"
+        class="mr-2 ml-2"
+        placeholder="Cpf"
+      ></b-form-input>
+    </b-form-group>
 
+    <b-form-group id="input-group-6" label="Email: " label-for="input-6">
+      <b-form-input
+        id="input-6"
+        v-model="email"
+        class="mr-2 ml-2"
+        placeholder="Email: "
+        type="email"
+      ></b-form-input>
+    </b-form-group>
 
-       
-        <b-button variant="primary" @click="cadastrar">Salvar</b-button>
-    
-      <b-list-group
-        inline
-        class="lista mt-3 ml-3"
-        v-for="aluno in alunos"
-        :key="aluno.id"
-      >
-        <b-list-group-item>
-          <input
-            class="elementoLista"
-            v-model="aluno.nome"
-            :disabled="desabilita"
-          />
-        </b-list-group-item>
-        <div>
-          <b-button variant="info" class="botao mr-2" @click="desabilita = false"
-            >Editar</b-button
-          >
-          <b-button variant="success" class="botao mr-2" @click="atualizar(aluno)"
-            >Atualizar</b-button
-          >
-          <b-button variant="danger" class="botao" @click="remover(aluno.id)"
-            >Excluir</b-button
-          >
-        </div>
-      </b-list-group>
-    </b-form>  
+    <b-form-group
+      id="input-group-7"
+      label="Data de pagamento: "
+      label-for="input-7"
+    >
+      <b-form-input
+        id="input-7"
+        v-model="dataDePagamento"
+        class="mr-2 ml-2"
+        placeholder="Data de Pagamento: "
+        type="date"
+      ></b-form-input>
+    </b-form-group>
+    <b-button variant="primary" @click="cadastrar">Salvar</b-button>
+
+    <b-list-group
+      class="lista mt-3 ml-3"
+      v-for="aluno in alunos"
+      :key="aluno.id"
+    >
+      <b-list-group-item>
+        <input
+          class="elementoLista"
+          v-model="aluno.nome"
+          :disabled="desabilita"
+        />
+      </b-list-group-item>
+      <div>
+        <b-button variant="info" class="botao mr-2" @click="desabilita = false"
+          >Editar</b-button
+        >
+        <b-button variant="success" class="botao mr-2" @click="atualizar(aluno)"
+          >Atualizar</b-button
+        >
+        <b-button variant="danger" class="botao" @click="remover(aluno.id)"
+          >Excluir</b-button
+        >
+      </div>
+    </b-list-group>
   </div>
 </template>
 
@@ -86,6 +111,12 @@ export default {
     return {
       alunos: null,
       nome: "",
+      sobrenome: "",
+      dataDeNascimento: "",
+      sexo: "",
+      cpf: "",
+      email: "",
+      dataDePagamento: "",
       desabilita: true,
     };
   },
@@ -131,24 +162,12 @@ export default {
 </script>
 
 <style>
-
-/* .lista {
-  max-width: 400px;
+/* .formulario {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 }
 
-.botao {
-  max-width: 100px;
-  display: inline;
-}
-
-.elementoLista {
-  width: 300px;
-  background-color: white;
-  border: none;
-}
-
-.elementoLista:focus {
-  box-shadow: 0 0 0 0;
-  outline: 0;
+.tamanho {
+  width: 50%;
 } */
 </style>
